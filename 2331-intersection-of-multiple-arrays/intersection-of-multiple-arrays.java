db73b58c06130bf386a0c82275300bd0db88e91a
kutluhana <56555567+kutluhana@users.noreply.github.com>
2026-09-04 19:04:18 +0300

    Time: 5 ms (48.10%) | Memory: 46.6 MB (62.12%) - LeetSync

---
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        
+        int[] existingNumbers = new int[nums.length];
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums[i].length; j++) {
+                map.put(nums[i][j], map.getOrDefault(nums[i][j], 0) + 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == nums.length) {
+                list.add(entry.getKey());
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
