1d5e02fc9833aa6e5239317e97e79ce9a6c66358
kutluhana <56555567+kutluhana@users.noreply.github.com>
2026-09-06 19:23:15 +0300

    Time: 37 ms (47.87%) | Memory: 54 MB (57.45%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,21 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int equalPairs(int[][] grid) {
-        int n = grid.length;
-        int count = 0;
+        int matrixSize = grid.length;
+        Map<String, Integer> rowFrequencyMap = new HashMap<>();
 
-        for (int c = 0; c < n; c++) {
-            int[] col = new int[n];
-            for (int r = 0; r < n; r++) {
-                col[r] = grid[r][c];
-            }
-
-            for (int r = 0; r < n; r++) {
-                if (Arrays.equals(grid[r], col)) {
-                    count++;
-                }
-            }
+        for (int[] currentRow : grid) {
+            String rowPattern = Arrays.toString(currentRow);
+            rowFrequencyMap.put(rowPattern, rowFrequencyMap.getOrDefault(rowPattern, 0) + 1);
         }
 
-        return count;
+        int matchingPairCount = 0;
+
+        for (int colIndex = 0; colIndex < matrixSize; colIndex++) {
+            int[] currentColumn = extractColumn(grid, colIndex, matrixSize);
+            String columnPattern = Arrays.toString(currentColumn);
+
+            matchingPairCount += rowFrequencyMap.getOrDefault(columnPattern, 0);
+        }
+
+        return matchingPairCount;
+    }
+
+    private int[] extractColumn(int[][] grid, int colIndex, int matrixSize) {
+        int[] column = new int[matrixSize];
+        for (int rowIndex = 0; rowIndex < matrixSize; rowIndex++) {
+            column[rowIndex] = grid[rowIndex][colIndex];
+        }
+        return column;
     }
 }
