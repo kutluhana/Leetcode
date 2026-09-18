48a59d7b35d15e71b4943d1b1a64690455ea4618
kutluhana <56555567+kutluhana@users.noreply.github.com>
2026-09-18 21:19:21 +0300

    Time: 249 ms (5.07%) | Memory: 166.7 MB (5.02%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,54 +1,47 @@
 class Solution {
+
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    Set<Integer> set = new HashSet<>();
     Set<String> roads = new HashSet<>();
-    Map<Integer, List<Integer>> map = new HashMap<>();;
-    Set<Integer> seen = new HashSet<>();
+    int answer = 0;
+
     public int minReorder(int n, int[][] connections) {
-
+        
         for(int i = 0; i < connections.length; i++) {
-            for(int j = 0; j < 2; j++) {
+            List<Integer> list1 = map.getOrDefault(connections[i][0], new ArrayList<>());
+            List<Integer> list2 = map.getOrDefault(connections[i][1], new ArrayList<>());
 
-                if(map.containsKey(connections[i][0])) {
-                    map.get(connections[i][0]).add(connections[i][j]);
-                } else {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(connections[i][j]);
-                    map.put(connections[i][0], list);
-                }
+            list1.add(connections[i][1]);
+            list2.add(connections[i][0]);
 
-                if(map.containsKey(connections[i][1])) {
-                    map.get(connections[i][1]).add(connections[i][j]);
-                } else {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(connections[i][j]);
-                    map.put(connections[i][1], list);
-                }
-
-                roads.add(convertToHash(connections[i][0], connections[i][1]));
-            }
+            map.put(connections[i][0], list1);
+            map.put(connections[i][1], list2);
+            roads.add(convertToHash(connections[i][0], connections[i][1]));
         }
 
-        seen.add(0);
-        return dfs(0);
-        
+        dfs(0);
+
+        return answer;
     }
 
-    public int dfs(int node) {
-        int ans = 0;
-        for (int neighbor: map.get(node)) {
-            if (!seen.contains(neighbor)) {
-                if (roads.contains(convertToHash(node, neighbor))) {
-                    ans++;
-                }
-
-                seen.add(neighbor);
-                ans += dfs(neighbor);
-            }
+    void dfs(int city) {
+        if(set.contains(city)) {
+            return;
         }
-        
-        return ans;
+
+        set.add(city);
+
+        for(Integer connected : map.get(city)) {
+            if (!set.contains(connected) && roads.contains(convertToHash(city, connected))) {
+                answer++;
+            }
+            dfs(connected);
+        }
     }
 
     public String convertToHash(int row, int col) {
         return String.valueOf(row) + "," + String.valueOf(col);
     }
+
+
 }
